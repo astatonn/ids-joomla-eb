67a173c7859e6bdc10c555136e3cd238ea154d95
astatonn <lucas.lima.rk@gmail.com>
2024-04-16 13:38:46 -0300

    ✨🚀incluindo ícones no modolinks

--- modules/mod_links/mod_links.php
+++ modules/mod_links/mod_links.php
@@ -24,6 +24,7 @@ use Joomla\CMS\Helper\ModuleHelper;
                 <a href="<?php echo $params['url-link-' . $i]; ?>" target="_blank" class="link-link">
                     <div class="link-card br-card">
                         <div class="link-content">
+                            <i class="<?php echo $params['icon-link-'. $i ]?>" style="color:var(--green-vivid-60);font-size:40px;margin:10px 0"></i>
                             <h5 class="link-title"><?php echo $params['name-link-' . $i]; ?></h5>
                         </div>
                     </div>
