94c5e111fed1f0e95fc57c60c0b75f8098ba54e3
Lucas Lima <57077913+astatonn@users.noreply.github.com>
2024-11-11 14:49:08 -0300

    Update mod_links.php

--- modules/mod_links/mod_links.php
+++ modules/mod_links/mod_links.php
@@ -1,16 +1,5 @@
-<?php
-defined('_JEXEC') or die;
-
-use Joomla\CMS\Helper\ModuleHelper;
-use Joomla\CMS\Factory;
-
-$app = Factory::getApplication();
-
-?>
-
 <div class="container">
     <!-- HEADER  -->
-
     <div class="autofit-float autofit-row portlet-header">
         <div class="autofit-col autofit-col-expand">
             <div class="portlet-title-text">
@@ -19,13 +8,13 @@ $app = Factory::getApplication();
         </div>
     </div>
 
-
     <div class="links-container<?php echo ($params['compact-links'] == 1) ? " links-compact": "";?>">
         <?php for ($i = 1; $i <= $params['link-amount'] ; $i++) : ?>
             <div class="link-col">
                 <a href="<?php echo $params['url-link-' . $i]; ?>" target="_blank" class="link-link">
                     <div class="link-card br-card">
                         <div class="link-content">
+                            <!-- Exibe o ícone somente se o compact-links estiver desativado -->
                             <?php if($params['compact-links'] == 0) : ?> 
                                 <i class="<?php echo $params['icon-link-'. $i ]?> tema-<?php echo $app->getConfig()['themeParams']['tema']; ?>-third" style="font-size:40px;margin:10px 0"></i>
                             <?php endif; ?>    
@@ -36,6 +25,4 @@ $app = Factory::getApplication();
             </div>
         <?php endfor; ?>
     </div>
-
-
 </div>
