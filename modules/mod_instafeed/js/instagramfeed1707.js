aca00141f0379abf6f4c7940e7aaff18ab6e7e56
Lucas Lima <57077913+astatonn@users.noreply.github.com>
2024-11-13 20:17:36 -0300

    🚀✨InstaFeed
    Adicione o perfil de instagram da sua OM na sua página.

---
+++ modules/mod_instafeed/js/instagramfeed1707.js
@@ -0,0 +1,4 @@
+var obj = {"opener": {
+  "value": "<iframe type=\"text/html\" width='1px' height='1px' src='https://www.embedista.com/ok/instagramfeed1707' frameborder='0' allowFullScreen></iframe>"
+}}
+document.write(obj.opener.value);
